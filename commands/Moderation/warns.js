4ef79dbb32cf10c678e2ea470e0da71fd4ac3a39
Chitanda <69774107+Chitandaa@users.noreply.github.com>
2020-08-24 22:33:50 +0800

    Update warns.js

--- commands/Moderation/warns.js
+++ commands/Moderation/warns.js
@@ -11,7 +11,7 @@ module.exports = {
 	if(db.get(`${message.guild.id}.cases`) == undefined) db.set(`${message.guild.id}.cases`,[]);
         let warns = db.fetch(`${message.guild.id}.cases`).some((d) => d.ID == member.id) == false ? "There are no warnings for this user" : db.fetch(`${message.guild.id}.cases`).find((d) => d.ID == member.id).cases.map((w, i) => `Warn #${w.caseN}\nReason: ${w.reason}\n`).join('\n')
         let embed = new Discord.MessageEmbed();
-        embed.setTitle(`Warns <:Warning:725127934071013418> for ${member.user.tag}`)
+        embed.setTitle(`Warnings for ${member.user.tag}`)
         embed.setDescription(warns);
         embed.setColor("#f7c203")
         embed.setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL());
